2b79ccb4fcb46cb5ad2d08a18838b3c1a48c3e18
stray-code <abc1007020@gmail.com>
2024-05-03 15:08:26 +0900

    crxjs追加と設定

---
+++ vite.config.ts
@@ -0,0 +1,25 @@
+import { defineConfig } from "vite";
+import { crx, defineManifest } from "@crxjs/vite-plugin";
+
+const manifest = defineManifest({
+  manifest_version: 3,
+  name: "ページズーム",
+  description: "1クリックでページをズームします。",
+  version: "1.0.0",
+  icons: {
+    16: "img/icon16.png",
+    48: "img/icon48.png",
+    128: "img/icon128.png",
+  },
+});
+
+export default defineConfig({
+  plugins: [crx({ manifest })],
+  server: {
+    port: 5173,
+    strictPort: true,
+    hmr: {
+      port: 5173,
+    },
+  },
+});
