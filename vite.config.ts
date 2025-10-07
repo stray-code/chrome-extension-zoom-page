023415bc894a35ec89c9519c3ee57ba81145cd5b
stray-code <abc1007020@gmail.com>
2025-10-07 23:46:15 +0900

    format

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,18 @@
-import { defineConfig } from "vite";
-import { crx, defineManifest } from "@crxjs/vite-plugin";
+import { crx, defineManifest } from '@crxjs/vite-plugin';
+import { defineConfig } from 'vite';
 
 const manifest = defineManifest({
   manifest_version: 3,
-  name: "ページズーム",
-  description: "1クリックでページをズームします。",
-  version: "1.0.0",
+  name: 'ページズーム',
+  description: '1クリックでページをズームします。',
+  version: '1.0.0',
   icons: {
-    16: "img/icon16.png",
-    48: "img/icon48.png",
-    128: "img/icon128.png",
+    16: 'img/icon16.png',
+    48: 'img/icon48.png',
+    128: 'img/icon128.png',
   },
   action: {
-    default_popup: "src/popup/index.html",
+    default_popup: 'src/popup/index.html',
   },
 });
 
